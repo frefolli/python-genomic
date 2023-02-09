69144209b175d9534f16175b40c2e3c9416f630c
frefolli <f.refolli@campus.unimib.it>
2023-02-09 11:57:10 +0100

    update main.py with reference code load

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # import pysam
 import pysam, re
+from Bio import SeqIO
+
+# define reference import function
+def open_reference(path):
+    reference = SeqIO.index(path, "fasta")
+    return reference['X']
 
 # define samfile import function
 def open_samfile(path):
@@ -18,7 +24,9 @@ def extract_candidates(samfile):
 if __name__ == "__main__":
     samfile = open_samfile('./sample.bam')
     candidates = [_ for _ in extract_candidates(samfile)]
-    print(f"Table Length: {len(candidates)}")
-    for i in range(20):
-        print(candidates[i])
-    print("... ... ... ...")
+    reference = open_reference('GRCh37.X.fasta')
+
+    c = candidates[0]
+    print(c.cigarstring)
+    print(c.query_sequence.upper())
+    print(reference[c.reference_start: c.reference_end].seq.upper())
